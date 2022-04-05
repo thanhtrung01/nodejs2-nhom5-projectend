9a318171605a7003a86896612b5006d2754ba9b2
phong47047 <phong47047@donga.edu.vn>
2022-04-05 23:01:42 +0700

    cart update
    update cart-shopping

--- frontend/client/src/pages/CartPage.jsx
+++ frontend/client/src/pages/CartPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function CartPage(props) {
-	return <div>CartPage HEllo World</div>;
+	return <div>CartPage Heloo this is carrt shop</div>;
 }
 
 CartPage.propTypes = {};
