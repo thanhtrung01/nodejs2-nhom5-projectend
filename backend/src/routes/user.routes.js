6dcfd8fdc1ad38c0d58ef09f2533e286289af8f8
Quốc Lê <quoc020721@gmail.com>
2022-06-02 10:29:44 +0700

    update edit user

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -7,7 +7,8 @@ const { isAuth } = require('../middlewares/authentication');
 router.get('/', userCTRL.getUsers);
 router.get('/:userId', userCTRL.getUser);
 router.post('/', isAuth, userCTRL.createUser);
-router.put('/:userId', isAuth, userCTRL.updateUser);
-router.delete('/:userId', isAuth, userCTRL.deleteUser);
+// router.put('/:userId', isAuth, userCTRL.updateUser);
+router.put('/:userId', userCTRL.updateUser);
+router.delete('/:userId', userCTRL.deleteUser);
 
 module.exports = router;
